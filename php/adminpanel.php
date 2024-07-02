38627fe787679f3f242d91ac68008645ae2b24d4
StevenMadali <stevenmadali17@gmail.com>
2024-07-02 21:30:23 +0800

    early draft for adminpanel

--- php/adminpanel.php
+++ php/adminpanel.php
@@ -1,14 +1,14 @@
 
 <?php
 
-session_start();
+// session_start();
 
-if(!isset($_SESSION["username"])) { // Check if username session variable is set
-  header("location:admin.php");   // Redirect to admin.php if not set
+// if(!isset($_SESSION["username"])) { // Check if username session variable is set
+//   header("location:admin.php");   // Redirect to admin.php if not set
                     
-}
+// }
 
-// Code to display admin panel content would be here
+// // Code to display admin panel content would be here
 
 ?>
 
@@ -19,17 +19,20 @@ if(!isset($_SESSION["username"])) { // Check if username session variable is set
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Panel</title>
+    <link rel="stylesheet" href="../css/adminpanel.css">
+    <link rel="shortcut icon" href="../images/logo.png" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
 </head>
 <body>
 
     
-    <h1>Hello world</h1>
-    <?php
-    
-    echo $_SESSION["admin"]
-    ?>
-    
+    <div class="typecontainer">
+        <span class="text myText">Welcome back Steven!</span>
+    </div>
 
-    <a href="logout.php">Logout</a>
+    <button><a href="logout.php">Logout</a></button>
 </body>
 </html>

23b8d95a2f05434ce7d3b9fe4a838dd4dcb7ca85
StevenMadali <stevenmadali17@gmail.com>
2024-09-01 01:11:06 +0800

    major changes on aihelpershome UI

--- php/aihelpershome.php
+++ php/aihelpershome.php
@@ -1,98 +1,504 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Linkorage</title>
-    <link rel="stylesheet" href="../css/editaihelpers.css">
     <link rel="shortcut icon" href="../images/logo.png" type="image/x-icon">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/f60f4be67d.js" crossorigin="anonymous"></script>
 
-    <script src="https://cdn.tailwindcss.com?plugins=forms,typography"></script>
-	<script src="https://unpkg.com/unlazy@0.11.3/dist/unlazy.with-hashing.iife.js" defer init></script>
+      <!-- FONT AWESOME LINK -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <script type="text/javascript">
-			window.tailwind.config = {
-				darkMode: ['class'],
-				theme: {
-					extend: {
-						colors: {
-							border: 'hsl(var(--border))',
-							input: 'hsl(var(--input))',
-							ring: 'hsl(var(--ring))',
-							background: '#252525',
-							foreground: 'hsl(var(--foreground))',
-							primary: {
-								DEFAULT: 'hsl(var(--primary))',
-								foreground: 'hsl(var(--primary-foreground))'
-							},
-							secondary: {
-								DEFAULT: 'hsl(var(--secondary))',
-								foreground: 'hsl(var(--secondary-foreground))'
-							},
-							destructive: {
-								DEFAULT: 'hsl(var(--destructive))',
-								foreground: 'hsl(var(--destructive-foreground))'
-							},
-							muted: {
-								DEFAULT: 'hsl(var(--muted))',
-								foreground: 'hsl(var(--muted-foreground))'
-							},
-							accent: {
-								DEFAULT: 'hsl(var(--accent))',
-								foreground: 'hsl(var(--accent-foreground))'
-							},
-							popover: {
-								DEFAULT: 'hsl(var(--popover))',
-								foreground: 'hsl(var(--popover-foreground))'
-							},
-							card: {
-								DEFAULT: 'hsl(var(--card))',
-								foreground: 'hsl(var(--card-foreground))'
-							},
-						},
-					}
-				}
-			}
-		</script>
-    
+    <style>
+      @import url(https://fonts.googleapis.com/css2?family=Montserrat&display=swap);
+      
+      /*! tailwindcss v3.4.3 | MIT License | https://tailwindcss.com*/
+      *,
+      :after,
+      :before {
+        border: 0 solid #e5e7eb;
+        box-sizing: border-box;
+      }
+      :after,
+      :before {
+        --tw-content: "";
+      }
+      :host,
+      html {
+        line-height: 1.5;
+        -webkit-text-size-adjust: 100%;
+        font-family:
+          Oswald,
+          ui-sans-serif,
+          system-ui,
+          sans-serif,
+          Apple Color Emoji,
+          Segoe UI Emoji,
+          Segoe UI Symbol,
+          Noto Color Emoji;
+        font-feature-settings: normal;
+        font-variation-settings: normal;
+        -moz-tab-size: 4;
+        tab-size: 4;
+        -webkit-tap-highlight-color: transparent;
+      }
+      body {
+        line-height: inherit;
+        margin: 0;
+      }
+      hr {
+        border-top-width: 1px;
+        color: inherit;
+        height: 0;
+      }
+      abbr:where([title]) {
+        text-decoration: underline dotted;
+      }
+      h1,
+      h2,
+      h3,
+      h4,
+      h5,
+      h6 {
+        font-size: inherit;
+        font-weight: inherit;
+      }
+      a {
+        color: inherit;
+        text-decoration: inherit;
+      }
+      b,
+      strong {
+        font-weight: bolder;
+      }
+      code,
+      kbd,
+      pre,
+      samp {
+        font-family:
+          ui-monospace,
+          SFMono-Regular,
+          Menlo,
+          Monaco,
+          Consolas,
+          Liberation Mono,
+          Courier New,
+          monospace;
+        font-feature-settings: normal;
+        font-size: 1em;
+        font-variation-settings: normal;
+      }
+      small {
+        font-size: 80%;
+      }
+      sub,
+      sup {
+        font-size: 75%;
+        line-height: 0;
+        position: relative;
+        vertical-align: baseline;
+      }
+      sub {
+        bottom: -0.25em;
+      }
+      sup {
+        top: -0.5em;
+      }
+      table {
+        border-collapse: collapse;
+        border-color: inherit;
+        text-indent: 0;
+      }
+      button,
+      input,
+      optgroup,
+      select,
+      textarea {
+        color: inherit;
+        font-family: inherit;
+        font-feature-settings: inherit;
+        font-size: 100%;
+        font-variation-settings: inherit;
+        font-weight: inherit;
+        letter-spacing: inherit;
+        line-height: inherit;
+        margin: 0;
+        padding: 0;
+      }
+      button,
+      select {
+        text-transform: none;
+      }
+      button,
+      input:where([type="button"]),
+      input:where([type="reset"]),
+      input:where([type="submit"]) {
+        -webkit-appearance: button;
+        background-color: transparent;
+        background-image: none;
+      }
+      :-moz-focusring {
+        outline: auto;
+      }
+      :-moz-ui-invalid {
+        box-shadow: none;
+      }
+      progress {
+        vertical-align: baseline;
+      }
+      ::-webkit-inner-spin-button,
+      ::-webkit-outer-spin-button {
+        height: auto;
+      }
+      [type="search"] {
+        -webkit-appearance: textfield;
+        outline-offset: -2px;
+      }
+      ::-webkit-search-decoration {
+        -webkit-appearance: none;
+      }
+      ::-webkit-file-upload-button {
+        -webkit-appearance: button;
+        font: inherit;
+      }
+      summary {
+        display: list-item;
+      }
+      blockquote,
+      dd,
+      dl,
+      figure,
+      h1,
+      h2,
+      h3,
+      h4,
+      h5,
+      h6,
+      hr,
+      p,
+      pre {
+        margin: 0;
+      }
+      fieldset {
+        margin: 0;
+      }
+      fieldset,
+      legend {
+        padding: 0;
+      }
+      menu,
+      ol,
+      ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+      }
+      dialog {
+        padding: 0;
+      }
+      textarea {
+        resize: vertical;
+      }
+      input::placeholder,
+      textarea::placeholder {
+        color: #9ca3af;
+        opacity: 1;
+      }
+      [role="button"],
+      button {
+        cursor: pointer;
+      }
+      :disabled {
+        cursor: default;
+      }
+      audio,
+      canvas,
+      embed,
+      iframe,
+      img,
+      object,
+      svg,
+      video {
+        display: block;
+        vertical-align: middle;
+      }
+      img,
+      video {
+        height: auto;
+        max-width: 100%;
+      }
+      [hidden] {
+        display: none;
+      }
+      *,
+      :after,
+      :before {
+        --tw-border-spacing-x: 0;
+        --tw-border-spacing-y: 0;
+        --tw-translate-x: 0;
+        --tw-translate-y: 0;
+        --tw-rotate: 0;
+        --tw-skew-x: 0;
+        --tw-skew-y: 0;
+        --tw-scale-x: 1;
+        --tw-scale-y: 1;
+        --tw-pan-x: ;
+        --tw-pan-y: ;
+        --tw-pinch-zoom: ;
+        --tw-scroll-snap-strictness: proximity;
+        --tw-gradient-from-position: ;
+        --tw-gradient-via-position: ;
+        --tw-gradient-to-position: ;
+        --tw-ordinal: ;
+        --tw-slashed-zero: ;
+        --tw-numeric-figure: ;
+        --tw-numeric-spacing: ;
+        --tw-numeric-fraction: ;
+        --tw-ring-inset: ;
+        --tw-ring-offset-width: 0px;
+        --tw-ring-offset-color: #fff;
+        --tw-ring-color: rgba(59, 130, 246, 0.5);
+        --tw-ring-offset-shadow: 0 0 #0000;
+        --tw-ring-shadow: 0 0 #0000;
+        --tw-shadow: 0 0 #0000;
+        --tw-shadow-colored: 0 0 #0000;
+        --tw-blur: ;
+        --tw-brightness: ;
+        --tw-contrast: ;
+        --tw-grayscale: ;
+        --tw-hue-rotate: ;
+        --tw-invert: ;
+        --tw-saturate: ;
+        --tw-sepia: ;
+        --tw-drop-shadow: ;
+        --tw-backdrop-blur: ;
+        --tw-backdrop-brightness: ;
+        --tw-backdrop-contrast: ;
+        --tw-backdrop-grayscale: ;
+        --tw-backdrop-hue-rotate: ;
+        --tw-backdrop-invert: ;
+        --tw-backdrop-opacity: ;
+        --tw-backdrop-saturate: ;
+        --tw-backdrop-sepia: ;
+        --tw-contain-size: ;
+        --tw-contain-layout: ;
+        --tw-contain-paint: ;
+        --tw-contain-style: ;
+      }
+      ::backdrop {
+        --tw-border-spacing-x: 0;
+        --tw-border-spacing-y: 0;
+        --tw-translate-x: 0;
+        --tw-translate-y: 0;
+        --tw-rotate: 0;
+        --tw-skew-x: 0;
+        --tw-skew-y: 0;
+        --tw-scale-x: 1;
+        --tw-scale-y: 1;
+        --tw-pan-x: ;
+        --tw-pan-y: ;
+        --tw-pinch-zoom: ;
+        --tw-scroll-snap-strictness: proximity;
+        --tw-gradient-from-position: ;
+        --tw-gradient-via-position: ;
+        --tw-gradient-to-position: ;
+        --tw-ordinal: ;
+        --tw-slashed-zero: ;
+        --tw-numeric-figure: ;
+        --tw-numeric-spacing: ;
+        --tw-numeric-fraction: ;
+        --tw-ring-inset: ;
+        --tw-ring-offset-width: 0px;
+        --tw-ring-offset-color: #fff;
+        --tw-ring-color: rgba(59, 130, 246, 0.5);
+        --tw-ring-offset-shadow: 0 0 #0000;
+        --tw-ring-shadow: 0 0 #0000;
+        --tw-shadow: 0 0 #0000;
+        --tw-shadow-colored: 0 0 #0000;
+        --tw-blur: ;
+        --tw-brightness: ;
+        --tw-contrast: ;
+        --tw-grayscale: ;
+        --tw-hue-rotate: ;
+        --tw-invert: ;
+        --tw-saturate: ;
+        --tw-sepia: ;
+        --tw-drop-shadow: ;
+        --tw-backdrop-blur: ;
+        --tw-backdrop-brightness: ;
+        --tw-backdrop-contrast: ;
+        --tw-backdrop-grayscale: ;
+        --tw-backdrop-hue-rotate: ;
+        --tw-backdrop-invert: ;
+        --tw-backdrop-opacity: ;
+        --tw-backdrop-saturate: ;
+        --tw-backdrop-sepia: ;
+        --tw-contain-size: ;
+        --tw-contain-layout: ;
+        --tw-contain-paint: ;
+        --tw-contain-style: ;
+      }
+      #webcrumbs :is(.mx-auto) {
+        margin-left: auto;
+        margin-right: auto;
+      }
+      #webcrumbs :is(.mb-6) {
+        margin-bottom: 24px;
+      }
+      #webcrumbs :is(.mt-2) {
+        margin-top: 8px;
+      }
+      #webcrumbs :is(.mt-8) {
+        margin-top: 32px;
+      }
+      #webcrumbs :is(.min-w-full) {
+        min-width: 100%;
+      }
+      #webcrumbs :is(.max-w-2xl) {
+        max-width: 250rem;
+      }
+      #webcrumbs :is(.overflow-hidden) {
+        overflow: hidden;
+      }
+      #webcrumbs :is(.rounded-lg) {
+        border-radius: 8px;
+      }
+      #webcrumbs :is(.rounded-md) {
+        border-radius: 6px;
+      }
+      #webcrumbs :is(.border) {
+        border-width: 1px;
+      }
+      #webcrumbs :is(.border-t) {
+        border-top-width: 1px;
+      }
+      #webcrumbs :is(.border-neutral-200) {
+        --tw-border-opacity: 1;
+        border-color: rgb(27 33 39 / var(--tw-border-opacity));
+      }
+      #webcrumbs :is(.bg-neutral-50) {
+        --tw-bg-opacity: 1;
+        background-color: rgb(29 35 42 / var(--tw-bg-opacity));
+      }
+      #webcrumbs :is(.bg-primary) {
+        --tw-bg-opacity: 1;
+        background-color: rgb(116 127 255 / var(--tw-bg-opacity));
+      }
+      #webcrumbs :is(.p-6) {
+        padding: 50px;
+      }
+      #webcrumbs :is(.px-4) {
+        padding-left: 16px;
+        padding-right: 16px;
+      }
+      #webcrumbs :is(.py-2) {
+        padding-bottom: 8px;
+        padding-top: 8px;
+      }
+      #webcrumbs :is(.text-left) {
+        text-align: left;
+      }
+      #webcrumbs :is(.text-center) {
+        text-align: center;
+      }
+      #webcrumbs :is(.font-title) {
+        font-family:
+          Montserrat,
+          ui-sans-serif,
+          system-ui,
+          sans-serif,
+          Apple Color Emoji,
+          Segoe UI Emoji,
+          Segoe UI Symbol,
+          Noto Color Emoji;
+      }
+      #webcrumbs :is(.text-3xl) {
+        font-size: 33.75px;
+        line-height: 40.5px;
+      }
+      #webcrumbs :is(.text-lg) {
+        font-size: 20.25px;
+        line-height: 31.5px;
+      }
+      #webcrumbs :is(.text-sm) {
+        font-size: 15.75px;
+        line-height: 22.5px;
+      }
+      #webcrumbs :is(.text-neutral-50) {
+        --tw-text-opacity: 1;
+        color: rgb(29 35 42 / var(--tw-text-opacity));
+      }
+      #webcrumbs :is(.text-primary) {
+        --tw-text-opacity: 1;
+        color: rgb(116 127 255 / var(--tw-text-opacity));
+      }
+      #webcrumbs :is(.underline) {
+        text-decoration-line: underline;
+      }
+      #webcrumbs :is(.shadow) {
+        --tw-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px -1px rgba(0, 0, 0, 0.1);
+        --tw-shadow-colored: 0 1px 3px 0 var(--tw-shadow-color),
+          0 1px 2px -1px var(--tw-shadow-color);
+        box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000),
+          var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
+      }
+      #webcrumbs {
+        font-family: Montserrat !important;
+        font-size: 18px !important;
+        line-height: 27px !important;
+      }
+      #webcrumbs :is(.bg-neutral-50) {
+        color: hsla(0, 0%, 100%, 0.9) !important;
+      }
+      
+      body {
+        line-height: inherit;
+        margin: 0;
+        padding: 10%;
+        display: flex;
+        flex-direction: column;
+        width: 100vw;
+        height: 100vh;
+        align-items: center;
+        justify-content: center;
+        background-color: #9ca3af;
+      }
 
-</head>
-<body>
+      .tabledisplay{
+        width: 100%; 
+      }
+      .titlelabel{
+        background: skyblue;
+        border-radius: 0;
+      }
+        .table-container {
+        overflow-y: scroll;
+        max-height: 300px; /* Adjust the height as needed */
+    }
 
 
-    
+    </style>
+  </head>
+  <body>
 
-            <!-- NAVBAR -->
 
-    <nav>
-        <input type="checkbox" id="check">
-        <label for="check" class="checkbtn">
-            <i class="fas fa-bars"></i>
-        </label>
-        <label class="logo">Linkorage</label>
-        <ul>
-            <li><a class="active" href="../index.php">Home</a></li>
-            <!-- <li><a href="logout.php" >Logout</a></li> -->
-            
-        </ul>
-    </nav>
-
-    <div class="typecontainer">
-        <span class="text myText">A.I Helpers</span>
+  <div id="webcrumbs"> 
+    <div class="max-w-2xl mx-auto bg-neutral-50 shadow rounded-lg p-6">
+        <header class="mb-6 text-center">
+            <h1 class="text-3xl font-title text-primary">A.I Helpers 
+                <i class="fa fa-hand-paper-o" aria-hidden="true"></i></h1>
+            <p class="mt-2 text-lg">Your go-to platform for AI tools and resources.</p>
+        </header>
+      <div class="mb-6 text-center">
+        <button onclick="history.back() "class="bg-primary text-neutral-50 py-2 px-4 rounded-md">
+            <span class="material-symbols-outlined align-middle mr-2"></span>
+            <i class="fa fa-home" aria-hidden="true"></i>
+        </button>
     </div>
 
+  <div class="table-container">
 
-            <!-- INSERT FORM -->
-
-            <!-- ERROR VALIDATION -->
- 
-    
-
-   <?php
+  <?php
 
 
 // database connector
@@ -104,10 +510,10 @@ include("connections.php");
    $view_query = mysqli_query($connections, "SELECT * FROM tbl_aihelpersrecord");
 
 
-   echo "<table class=displaytbl>";
-   echo "<tr>
-          <td class=linktitle>Link</td>
-          <td class=detailtitle>Detail</td>
+   echo "<table class=tabledisplay>";
+   echo "<tr class=titlelabel>
+          <td class=titledetail>Detail</td>
+           <td class=titlelink>Link</td>
         </tr>";
           
   
@@ -119,8 +525,9 @@ include("connections.php");
       $db_link = $row["link"];
       $db_detail = $row["detail"];
       echo "<tr>
-               <td class=px-7 py-2 text-left text-secondary><a href='$db_link' target='_blank'>$db_link</a></td>
-               <td class=px-4 py-2 text-left text-secondary>$db_detail</td>
+               <td class=detailtext>$db_detail</td>
+               
+               <td class=linktext><a href='$db_link' target='_blank'>Visit</a></td>
             </tr>";
       
     }
@@ -130,5 +537,13 @@ include("connections.php");
 ?>
 
 
-</body>
+
+
+  </div>
+
+  
+    
+      
+  
+  </body>
 </html>
